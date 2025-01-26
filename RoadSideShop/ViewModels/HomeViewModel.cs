33c2013ba849f1de50ac10a4558583c4b69df16a
benito JD <benito.josede@gmail.com>
2025-01-26 09:44:20 +0530

    Refactor MainPage layout and add HomeViewModel
    Significantly modified MainPage.xaml:
    - Removed ScrollView and VerticalStackLayout.
    - Introduced Grid layout with two columns.
    - Added Labels for "Hello, Benito", "Categories List", "Menu Items List", and "Current Order Items".
    - Set Shell.NavBarIsVisible to False.
    
    Updated MainPage.xaml.cs:
    - Removed OnCounterClicked event handler.
    
    Updated Colors.xaml:
    - Added LightWheat color resource.
    
    Updated RoadSideShop.csproj:
    - Removed ViewModels folder reference.
    
    Added HomeViewModel.cs:
    - Created HomeViewModel class inheriting from ObservableObject.
    - Added _databaseService field and _categories observable property.
    - Implemented InitializeAsync method to fetch menu categories.

---
+++ RoadSideShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RoadSideShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadSideShop.ViewModels
+{
+    public partial class HomeViewModel : ObservableObject
+    {
+        private readonly DatabaseService _databaseService;
+
+        [ObservableProperty]
+        public MenuCategory[] _categories = [];
+
+        public HomeViewModel(DatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+        private bool _isInitialized;
+        public async ValueTask InitializeAsync()
+        {
+            if (_isInitialized)
+            {
+                return;
+            }
+            _isInitialized = true;
+            Categories = await _databaseService.GetMenuCategoriesAsync();
+        }
+    }
+}
